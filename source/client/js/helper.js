6791fc9ad698ee72667774c24949eaabfecbc7fa
kpurusho <karthik.purushothaman@gmail.com>
2014-01-10 21:52:14 +0530

    added bootstrap datepicker

---
+++ source/client/js/helper.js
@@ -0,0 +1,11 @@
+﻿Ember.Handlebars.helper('format-date', function (date) {
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (day < 10) {
+        day = "0" + day;
+    }
+    return "%@-%@-%@".fmt(day, month, date.getFullYear());
+});
