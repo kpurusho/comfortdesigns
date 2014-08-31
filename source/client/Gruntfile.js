24e1b8659325da02260159c24f22aa8ec0f74f3a
unknown <karthik.purushothaman@gmail.com>
2014-08-31 14:57:21 +0530

    setting up grunt uglify and watch

--- source/client/Gruntfile.js
+++ source/client/Gruntfile.js
@@ -7,13 +7,21 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         concat: config('concat'),
         jshint: config('jshint'),
-        emberTemplates: config('emberTemplates')
+        emberTemplates: config('emberTemplates'),
+        uglify: config('uglify'),
+        watch: {
+            files: ['templates/**/*.hbs','app/**/*.js'],
+            tasks: ['jshint', 'emberTemplates', 'concat']
+        }
     });
 
     //load plugins
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-ember-templates');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['concat', 'jshint', 'emberTemplates']);
+    grunt.registerTask('dist', ['concat', 'jshint', 'emberTemplates', 'uglify']);
+    grunt.registerTask('default', ['watch']);
 };
